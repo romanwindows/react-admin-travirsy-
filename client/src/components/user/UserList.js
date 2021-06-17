eb18a163b73769390d9cc8ff2b2bf0823f8893b7
romanwindows <esportstats0@gmail.com>
2021-06-17 20:56:52 +0300

    feat: init react-admin

---
+++ client/src/components/user/UserList.js
@@ -0,0 +1,21 @@
+import {
+    List, 
+    Datagrid, 
+    TextField, 
+    EditButton, 
+    DeleteButton
+} from "react-admin"
+
+const UserList = (props) => {
+    return (<List {...props}>
+        <Datagrid>
+            <TextField source='id' />
+            <TextField source='name' />
+            <TextField source='email' />
+            <EditButton basePath='/users' />
+            <DeleteButton basePath='/users' />
+        </Datagrid>
+    </List>)
+}
+
+export default UserList
