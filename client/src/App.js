e6c6c27cf9a20a973d771491d6c10f45e957a11c
romanwindows <esportstats0@gmail.com>
2022-01-24 12:39:04 +0200

    fix: remove comment1

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import UserList from "./components/user/UserList";
 import UserCreate from "./components/user/UserCreate";
 import UserEdit from "./components/user/UserEdit";
 
-//new comment
 //second comment
 //subBranch comment
 function App() {
